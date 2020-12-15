616bd2ce0a0edb319629f5c5d18c03ff75ca1cb4
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2020-12-14 22:33:47 -0800

    Initial Commit

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Sample : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 100))
+            {
+               agent.destination = hit.point;
+            }
+
+        }      
+    }
+}
